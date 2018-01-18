b74a5930653051c7b750fb5547cd3f99ab0f0780
madenityan <marinakorsyn@gmail.com>
2018-01-18 22:37:01 +0200

    added arrays (map) for components Services and Contact

--- src/ContactCard.js
+++ src/ContactCard.js
@@ -1,20 +1,40 @@
 import React from 'react';
 
 const ContactCard = () => {
+
+    const listSContact = [
+        {
+            titleCard: 'email',
+            className: 'contact-email contact-info',
+            classText: 'info@consultplus.com'
+        },
+        {
+            titleCard: 'call us',
+            className: 'contact-phone contact-info',
+            classText: '+91 8879 8767 990'
+        },
+        {
+            titleCard: 'carrer',
+            className: 'contact-carrer contact-info',
+            classText: 'consult@career.com'
+        }
+    ];
+
     return (
+
         <ul className="list-contact">
-            <li className="contact-email contact-info">
-                <h3 className="title">email</h3>
-                <p>info@consultplus.com</p>
-            </li>
-            <li className="contact-phone contact-info">
-                <h3 className="title">call us</h3>
-                <a href="tel:+91 8879 8767 990">+91 8879 8767 990</a>
-            </li>
-            <li className="contact-carrer contact-info">
-                <h3 className="title">carrer</h3>
-                <p>consult@career.com</p>
-            </li>
+
+            {
+                listSContact.map((item, index) => {
+                    return (
+                        <li className={item.className} key={index}>
+                            <h3 className="title">{item.titleCard}</h3>
+                            <p className="text-section">{item.classText}</p>
+                        </li>
+                    );
+                })
+            }
+
         </ul>
     );
 };
--- src/ServicesCard.js
+++ src/ServicesCard.js
@@ -5,27 +5,28 @@ const ServicesCard = () => {
     const listServices = [
         {
             titleCard: 'business consultation',
-            className: 'business-img',
+            className: 'consultation',
             classText: 'Many variations of passages of Lorem available, but the majority have suffered alteration in some form, by injected humour'
         },
         {
             titleCard: 'boost local business',
-            className: 'boost-img',
+            className: 'local-business',
             classText: 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some'
         },
         {
             titleCard: 'accounting',
-            className: 'accounting-img',
+            className: 'accounting',
             classText: 'Professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur'
         }
     ];
+
     return (
 
         <ul className="list-services">
             {
                 listServices.map((item, index) => {
                     return (
-                        <li className="consultation" key={index}>
+                        <li className={item.className} key={index}>
                             <h3 className="title">{item.titleCard}</h3>
                             <p className="text-section">{item.classText}</p>
                         </li>
@@ -34,7 +35,6 @@ const ServicesCard = () => {
             }
 
         </ul>
-
 
     );
 };
