ea52c4417c7a1475f2f85d70f20b4a9f01d010cf
madenityan <marinakorsyn@gmail.com>
2018-01-26 23:35:59 +0200

    fixed Main

--- src/Main.js
+++ src/Main.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import { Router, Route } from 'react-router-dom';
-import createBrowserHistory from 'history/createBrowserHistory';
 
 //Components
 
@@ -16,19 +15,15 @@ import Pricing from './Pages/Pricing';
 class Main extends React.Component {
     render() {
 
-        const BrowserHistory = createBrowserHistory();
-
         return (
-            <Router history={BrowserHistory}>
                 <div>
-                <Route exact path="/" component={Home}/>
-                <Route path="/about" component={About}/>
-                <Route path="/industry" component={Industry}/>
-                <Route path="/pricing" component={Pricing}/>
-                <Route path="/careers" component={Careers}/>
-                <Route path="/contact" component={Contact}/>
+                    <Route exact path="/" component={Home}/>
+                    <Route path="/about" component={About}/>
+                    <Route path="/industry" component={Industry}/>
+                    <Route path="/pricing" component={Pricing}/>
+                    <Route path="/careers" component={Careers}/>
+                    <Route path="/contact" component={Contact}/>
                 </div>
-            </Router>
         );
     }
 }
